2e78940c1b70195df7a429225ec293810fce0512
jaina0218 <163209067+jaina0218@users.noreply.github.com>
2024-10-15 20:00:56 -0300

    Update informaçõesGlobais.js

--- graficos/informaçõesGlobais.js
+++ graficos/informaçõesGlobais.js
@@ -1,3 +1,10 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+onst url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+const parágrafo = document.createElement ('P')
+parágrafo.clossList.add('gráficos_container__texto')
+}
+
+vizualizarInformacoesGlobais()
